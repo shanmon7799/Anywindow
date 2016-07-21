7c6761e8626e702aaec8fcd0ed517e3afc7b4d84
shanmon7799 <shanmon7799@hotmail.com>
2016-07-21 18:56:46 +0800

    quote can random show in the view

--- app/controllers/windows_controller.rb
+++ app/controllers/windows_controller.rb
@@ -4,11 +4,11 @@ class WindowsController < ApplicationController
 
   def index
     @windows = Window.all
-
   end
 
   def show
     @window = Window.find(params[:id])
     @window.increment!(:page_views, 1)
+    @quote = Quote.order("RANDOM()").limit(1).first
 	end
 end
